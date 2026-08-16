c7ae379fca95642cf074d83f1f618f541ef3cb20
Ivy Xu <fakeshadow1337@gmail.com>
2026-08-16 23:26:54 +0800

    Add comments to Setting.cs

--- src/Models/Utils/Setting.cs
+++ src/Models/Utils/Setting.cs
@@ -10,6 +10,7 @@ public static class Setting
         "Leagues"
     );
 
+    // Create the setting.json under the %APPDATA%
     private static readonly string SettingPath = Path.Combine(AppDataFolder, "settings.json");
 
     private static readonly JsonSerializerOptions Option = new() { WriteIndented = true };
@@ -19,15 +20,22 @@ public static class Setting
         public bool AutoAccept { get; set; }
     }
 
+    /// <summary>
+    /// If Config is changed, UpdateSetting() should be explicitly called asynchronously
+    /// to save the changes to the settings.json file.
+    /// </summary>
     public static AppConfig? Config { get; set; } = LoadSetting();
 
     private static AppConfig? LoadSetting()
     {
+        // If file does not exist, create a empty file then return a new AppConfig instance.
         if (!File.Exists(SettingPath))
         {
             return new AppConfig();
         }
 
+        // If the file exists, attempt to read and deserialize it.
+        // If deserialization fails, return a new AppConfig instance.
         try
         {
             return JsonSerializer.Deserialize<AppConfig>(File.ReadAllText(SettingPath), Option);
@@ -38,6 +46,9 @@ public static class Setting
         }
     }
 
+    /// <summary>
+    /// Should be explicitly called whenever the Config is changed.
+    /// </summary>
     public static async Task UpdateSetting()
     {
         var updatedContent = JsonSerializer.Serialize(Config, Option);
